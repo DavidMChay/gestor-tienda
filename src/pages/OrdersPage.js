ed1f774edb3e58e32c265dc65d659d583a067871
David Chay <yapperdark2@gmail.com>
2024-06-27 07:40:46 -0600

    chore: Add react-router-dom package and implement routing in App component

---
+++ src/pages/OrdersPage.js
@@ -0,0 +1,12 @@
+// src/pages/OrdersPage.js
+import React from 'react';
+
+const OrdersPage = () => {
+  return (
+    <div>
+      <h1>Your Orders</h1>
+    </div>
+  );
+};
+
+export default OrdersPage;
---
+++ src/pages/ProductsPage.js
@@ -0,0 +1,12 @@
+// src/pages/ProductsPage.js
+import React from 'react';
+
+const ProductsPage = () => {
+  return (
+    <div>
+      <h1>Our Products</h1>
+    </div>
+  );
+};
+
+export default ProductsPage;
