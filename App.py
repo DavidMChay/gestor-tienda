d977c6cb9823e55379cd8d6b3e87e96d79de8ebc
David Chay <yapperdark2@gmail.com>
2024-06-28 09:43:09 -0600

    chore: Add base.html template for documentation and update main.html template

--- App.py
+++ App.py
@@ -2,6 +2,17 @@
 #1. Set-ExecutionPolicy Unrestricted -Scope Process
 #2. .\env\Scripts\activate
 #ACTIVAR SERVDOR DE PYTHON
-#1. python3 -m http.server
+#1. flask run --port 8000
 #Nota: Esto se hace dentro de la carpeta donde se encuentra el archivo App.py
 
+# app.py
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('main.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
