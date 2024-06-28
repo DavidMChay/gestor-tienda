53e4e46abff73bc0fd2540908a0ab4e87c363827
David Chay <yapperdark2@gmail.com>
2024-06-28 07:02:43 -0600

    actualizacion de app.py

---
+++ App.py
@@ -0,0 +1,7 @@
+#ACTIVAR ENTORNO VIRTUAL - TODOS LOS COMANDOS SE EJECUTAN EN LA TERMINAL
+#1. Set-ExecutionPolicy Unrestricted -Scope Process
+#2. .\env\Scripts\activate
+#ACTIVAR SERVDOR DE PYTHON
+#1. python3 -m http.server
+#Nota: Esto se hace dentro de la carpeta donde se encuentra el archivo App.py
+
