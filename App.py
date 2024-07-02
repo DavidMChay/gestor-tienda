fd71f8b86d749b5356c066a521ce9e94fd41966c
David Chay <yapperdark2@gmail.com>
2024-07-02 11:41:05 -0600

    chore: Update fonts and styles for login functionality

--- App.py
+++ App.py
@@ -8,6 +8,7 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, session
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import check_password_hash
+from werkzeug.utils import secure_filename
 import os
 
 app = Flask(__name__)
@@ -22,6 +23,7 @@ if not os.path.exists(os.path.join(basedir, 'database')):
 # Configuración de la base de datos
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'database', 'storage.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['UPLOAD_FOLDER'] = os.path.join(basedir, 'static', 'uploads')
 db = SQLAlchemy(app)
 
 # --MODELOS DE LAS TABLAS DE LA BASE DE DATOS--
@@ -33,6 +35,7 @@ class Producto(db.Model):
     precio = db.Column(db.Float, nullable=False)
     stock = db.Column(db.Integer, nullable=False)
     fecha_agregado = db.Column(db.DateTime, default=db.func.current_timestamp())
+    imagen = db.Column(db.String) 
 
 class Cliente(db.Model):
     __tablename__ = 'Clientes'
@@ -90,7 +93,53 @@ def productos():
 def pedidos():
     return render_template('orders.html')
 
+@app.route('/admin/panel')
+def admin_panel():
+    if 'admin_id' not in session:
+        return redirect(url_for('admin_login'))
+    return render_template('paneladmin.html')
+
+@app.route('/admin/panel/agregar')
+def admin_agregar():
+    if 'admin_id' not in session:
+        return redirect(url_for('admin_login'))
+    return render_template('adminagregarprod.html')
+
+
 # SOLICITUDES
+
+@app.route('/admin/panel/agregar', methods=['GET', 'POST'])
+def admin_agregar_producto():
+    if 'admin_id' not in session:
+        return redirect(url_for('admin_login'))
+    if request.method == 'POST':
+        nombre = request.form['nombre']
+        descripcion = request.form['descripcion']
+        precio = request.form['precio']
+        stock = request.form['stock']
+        imagen = request.files['imagen']
+
+        # Verificar si el producto ya existe
+        producto_existente = Producto.query.filter_by(nombre=nombre).first()
+        if producto_existente:
+            flash('El producto ya existe.')
+            return redirect(url_for('admin_agregar_producto'))
+
+        # Guardar la imagen en el servidor
+        if imagen:
+            filename = secure_filename(imagen.filename)
+            imagen.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            imagen_path = 'uploads/' + filename
+        else:
+            imagen_path = None
+
+        # Agregar nuevo producto a la base de datos
+        nuevo_producto = Producto(nombre=nombre, descripcion=descripcion, precio=precio, stock=stock, imagen=imagen_path)
+        db.session.add(nuevo_producto)
+        db.session.commit()
+        flash('Producto agregado exitosamente.')
+        return redirect(url_for('admin_panel'))
+    return render_template('adminagregarprod.html')
 
 @app.route('/admin_login', methods=['GET', 'POST'])
 def admin_login():
@@ -104,12 +153,6 @@ def admin_login():
         else:
             flash('Correo o contraseña incorrectos')
     return render_template('admin.html')
-
-@app.route('/admin/panel')
-def admin_panel():
-    if 'admin_id' not in session:
-        return redirect(url_for('admin_login'))
-    return render_template('paneladmin.html')
 
 @app.route('/logout')
 def logout():
